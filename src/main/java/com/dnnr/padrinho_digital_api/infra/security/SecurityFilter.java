59bce0247a8dc3135bb8d1d9a73abd589650ec90
raynan <raynan.araujo@gmail.com>
2025-10-26 20:53:49 -0300

    fix: recovery token

--- src/main/java/com/dnnr/padrinho_digital_api/infra/security/SecurityFilter.java
+++ src/main/java/com/dnnr/padrinho_digital_api/infra/security/SecurityFilter.java
@@ -36,7 +36,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
 
     private String recoverToken(HttpServletRequest request){
-        var authHeader = request.getHeader("Authorizarion");
+        var authHeader = request.getHeader("Authorization");
         if(authHeader == null) return null;
         return authHeader.replace("Bearer ", "");
     }
